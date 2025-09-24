9dc34ac63ff27895564f5b6a8e4e2fdf8a45e26e
Rohan <177009010+Rohan12190@users.noreply.github.com>
2025-09-25 01:54:26 +0530

    Website made for Bar to sell Cocktails

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
+import Contact from "./components/Contact.jsx";
+// import LocomotiveScroll from 'locomotive-scroll';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  // const locomotiveScroll = new LocomotiveScroll();
+
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
+  );
+};
+
+export default App;
